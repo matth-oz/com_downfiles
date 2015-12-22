bf508ffabc204d70eaa4022350bf22ff10bf7d99
Matvey Zemskov <matth_oz@mail.ru>
2015-12-22 17:13:08 +0300

    edit front-end template

--- site/views/downfiles/tmpl/default.php
+++ site/views/downfiles/tmpl/default.php
@@ -7,8 +7,9 @@ $document->addStyleSheet('/components/com_downfiles/css/downfiles.css');
 <?php if(!empty($this->items)):?>
 <ul class="downfiles-list">
     <?php foreach($this->items as $item):?>
+    <?php $ext = substr(strrchr($item->filename, '.'), 1);?>
     <li>
-        <a href="index.php?option=com_downfiles&task=item.getfile&file_id=<?php echo $item->id?>"><?php echo $item->file_desc?></a> (<?php echo $item->file_size;?>)
+        <a href="index.php?option=com_downfiles&task=item.getfile&file_id=<?php echo $item->id?>"><?php echo $item->file_desc?></a> (<?php echo $ext.", ".$item->file_size;?>)
     </li> 
     <?php endforeach;?>
 <?php endif;?>
