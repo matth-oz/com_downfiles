450e932189eeb440a359f97120610b2fc70842ec
matth_oz <matth_oz@mail.ru>
2018-08-23 20:16:33 +0300

    fixed bug when downloading zip archives

--- site/controllers/item.php
+++ site/controllers/item.php
@@ -16,7 +16,8 @@
          
             // получаем имя файла по ID
             $file_name = $model->GetFilePathByID($file_id);
-            $file_name = JPATH_SITE.'/'.$file_name;
+			$extension = strtolower(substr(strrchr($_GET['file'],"."),1));
+			$file_name = JPATH_SITE.'/'.$file_name;
 
             // если файл доступен
             if(JFile::exists($file_name)){
@@ -25,22 +26,37 @@
                 $date = date('Y-m-d H:i:s');
                 // сохраняем в переменную $ip_addr IP-адрес посетителя
                 $ip_addr = $_SERVER["REMOTE_ADDR"];
-
-	            header("Content-Type: application/force-download");
-	            header("Content-Type: application/download");
+				
+			  switch ($extension) {
+				case "txt": $ctype="text/plain"; break;
+				case "pdf": $ctype="application/pdf"; break;
+				case "exe": $ctype="application/octet-stream"; break;
+				case "zip": $ctype="application/zip"; break;
+				case "doc": $ctype="application/msword"; break;
+				case "xls": $ctype="application/vnd.ms-excel"; break;
+				case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+				case "gif": $ctype="image/gif"; break;
+				case "png": $ctype="image/png"; break;
+				case "jpeg": $ctype="image/jpg"; break;
+				case "jpg": $ctype="image/jpg"; break;
+				default: $ctype="application/force-download";
+			  }
+			  
+				header('Content-Type: '.$ctype.'; charset=utf-8');
 				header('Content-Disposition: attachment; filename='.basename($file_name));
-				//header("Content-Length: " . filesize($file_name));
-				header("Pragma: no-cache");
-				header("Expires: 0");
 
-                // сохраняем в БД id файла, дату, IP-адрес
-                if(readfile($file_name)){
+				ob_clean();
+				$read_res = readfile($file_name);			
+				
+				// сохраняем в БД id файла, дату, IP-адрес
+                if($read_res !== false){
                    $res =  $model->saveDownloadStatInfo($file_id, $date, $ip_addr);
                 }
                 else{
                    JFactory::getApplication()->enqueueMessage(JText::_('COM_DOWNFILES_FILE_NOT_EXIST'), 'error');
                    $this->setRedirect(JRoute::_('index.php?option=com_downfiles'));
-                }            
+                } 
+				exit();				
             }
             else{
                 JFactory::getApplication()->enqueueMessage(JText::_('COM_DOWNFILES_FILE_NOT_EXIST'), 'error');
